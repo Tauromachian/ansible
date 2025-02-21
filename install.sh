370cf47a8d3f43eb6d8fc20c5cad027fb1b3e560
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-02-21 09:29:02 -0500

    Auto confirm software properties common

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! command -v ansible > /dev/null 2>&1; then
-    sudo apt install software-properties-common
+    sudo apt install -y software-properties-common
     sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt update -y
     sudo apt install -y ansible
