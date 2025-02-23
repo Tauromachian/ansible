e3014b416f98684f6ae884c1050d27dd501b9c48
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-02-23 09:37:46 -0500

    Let myself know to not run this as sudo

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+if [ "$EUID" -eq 0 ]; then
+  echo "Error: This script should not be run with sudo or as root."
+  echo "Please run it as a regular user."
+  exit 1
+fi
+
 if ! command -v ansible > /dev/null 2>&1; then
     sudo apt install -y software-properties-common
     sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt update -y
     sudo apt install -y ansible
 fi
-
-ansible-playbook ansible.yml --ask-become-pass
