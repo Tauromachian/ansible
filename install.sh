b2b7b5b909794137383927a2879a0466ad4bf4c0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-05-21 19:29:31 -0400

    Ask for tags before installing

--- install.sh
+++ install.sh
@@ -6,6 +6,18 @@ if [ "$EUID" -eq 0 ]; then
   exit 1
 fi
 
+if [ $# -eq 0 ]; then
+    echo "Error: No arguments provided"
+    echo "Please enter a tag either home or work"
+    exit 1
+fi
+
+if [ "$1" != "home" ] && [ "$1" != "work" ]; then
+    echo "Error: Invalid argument"
+    echo "Please enter a tag either home or work"
+    exit 1
+fi
+
 if ! command -v ansible > /dev/null 2>&1; then
     sudo apt install -y software-properties-common
     sudo apt-add-repository -y ppa:ansible/ansible
@@ -13,4 +25,4 @@ if ! command -v ansible > /dev/null 2>&1; then
     sudo apt install -y ansible
 fi
 
-ansible-playbook ansible.yml --ask-become-pass
+ansible-playbook ansible.yml --ask-become-pass --tags $1
